c454b1058ba9e0d8bf175c4e4797aed258c1be16
Ardacan Süren <ardacansuren@gmail.com>
2025-12-08 18:15:07 +0300

    SQL bağlantısı.

--- Add.cs
+++ Add.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace StokTakip
 {
@@ -16,6 +17,7 @@ namespace StokTakip
         {
             InitializeComponent();
         }
+        SqlConnection conn = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=StokDB;Integrated Security=True");
 
         private void button1_Click(object sender, EventArgs e)
         {
