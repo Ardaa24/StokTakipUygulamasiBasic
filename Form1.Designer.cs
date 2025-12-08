7bb09e882687c39213ec2fdfe9d8acc8b7d23669
Ardacan Süren <ardacansuren@gmail.com>
2025-12-08 17:42:20 +0300

    UI Yenilikleri.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -201,6 +201,7 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(665, 87);
             this.dataGridView1.Name = "dataGridView1";
