823caf503a6f356bedb06653b7fd3e0972b8045c
Ardacan Süren <ardacansuren@gmail.com>
2025-12-12 10:55:07 +0300

    Stok Kontrol UI.

--- frmStock.Designer.cs
+++ frmStock.Designer.cs
@@ -31,17 +31,30 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.pnlProductControl = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.btnClose = new System.Windows.Forms.Button();
             this.panel4 = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnAddCut = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.panel2.SuspendLayout();
             this.panel4.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel6.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -79,6 +92,24 @@
             this.panel3.Size = new System.Drawing.Size(29, 43);
             this.panel3.TabIndex = 2;
             // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.MediumPurple;
+            this.btnClose.BackgroundImage = global::StokTakip.Properties.Resources.web_button_icon_back_button_free_png;
+            this.btnClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnClose.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(1361, 0);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(6, 9, 6, 9);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(84, 43);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
             // dataGridView1
             // 
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
@@ -99,45 +130,10 @@
             this.pnlProductControl.Size = new System.Drawing.Size(462, 1071);
             this.pnlProductControl.TabIndex = 7;
             // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.label2);
-            this.panel2.Location = new System.Drawing.Point(802, 68);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(311, 642);
-            this.panel2.TabIndex = 8;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(30, 39);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 28);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "label2";
-            // 
-            // btnClose
-            // 
-            this.btnClose.BackColor = System.Drawing.Color.MediumPurple;
-            this.btnClose.BackgroundImage = global::StokTakip.Properties.Resources.web_button_icon_back_button_free_png;
-            this.btnClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.btnClose.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btnClose.FlatAppearance.BorderSize = 0;
-            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnClose.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btnClose.ForeColor = System.Drawing.Color.White;
-            this.btnClose.Location = new System.Drawing.Point(1361, 0);
-            this.btnClose.Margin = new System.Windows.Forms.Padding(6, 9, 6, 9);
-            this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(84, 43);
-            this.btnClose.TabIndex = 0;
-            this.btnClose.UseVisualStyleBackColor = false;
-            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
-            // 
             // panel4
             // 
             this.panel4.Controls.Add(this.label3);
-            this.panel4.Location = new System.Drawing.Point(1133, 70);
+            this.panel4.Location = new System.Drawing.Point(816, 70);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(300, 642);
             this.panel4.TabIndex = 9;
@@ -151,14 +147,145 @@
             this.label3.TabIndex = 0;
             this.label3.Text = "label3";
             // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.btnAddCut);
+            this.panel2.Controls.Add(this.panel5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(1139, 70);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(294, 331);
+            this.panel2.TabIndex = 10;
+            // 
+            // btnAddCut
+            // 
+            this.btnAddCut.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnAddCut.Location = new System.Drawing.Point(0, 293);
+            this.btnAddCut.Name = "btnAddCut";
+            this.btnAddCut.Size = new System.Drawing.Size(292, 36);
+            this.btnAddCut.TabIndex = 4;
+            this.btnAddCut.Text = "Kampanya Ekle";
+            this.btnAddCut.UseVisualStyleBackColor = true;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel5.Location = new System.Drawing.Point(3, 46);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(308, 15);
+            this.panel5.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(181, 28);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Kampanya Yüzdesi";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 232);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(241, 34);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(90, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 28);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "KAMPANYA";
+            // 
+            // panel6
+            // 
+            this.panel6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel6.Controls.Add(this.btnDelete);
+            this.panel6.Controls.Add(this.panel7);
+            this.panel6.Controls.Add(this.label5);
+            this.panel6.Controls.Add(this.textBox2);
+            this.panel6.Controls.Add(this.label6);
+            this.panel6.Location = new System.Drawing.Point(1139, 419);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(297, 291);
+            this.panel6.TabIndex = 11;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel7.Location = new System.Drawing.Point(3, 46);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(308, 15);
+            this.panel7.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(74, 108);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 28);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Ürün Kodu";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(19, 139);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(241, 34);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(66, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(175, 28);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "SEÇİLİ ÜRÜNÜ SİL";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnDelete.Location = new System.Drawing.Point(0, 253);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(295, 36);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Ürünü Sil";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(77, 73);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(111, 28);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Ürün Kodu";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(22, 104);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(241, 34);
+            this.textBox3.TabIndex = 5;
+            // 
             // frmStock
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(1445, 724);
-            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel6);
             this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel4);
             this.Controls.Add(this.pnlProductControl);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.panel1);
@@ -172,10 +299,12 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -188,9 +317,21 @@
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Panel pnlProductControl;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnAddCut;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
--- frmStock.cs
+++ frmStock.cs
@@ -52,6 +52,6 @@ namespace StokTakip
             }
         }
 
-     
+
     }
 }
